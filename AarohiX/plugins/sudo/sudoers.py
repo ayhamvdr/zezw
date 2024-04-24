bf3955d4e514e8aa08464120cc1f1766436f451d
zoze <167098056+ayhamvdr@users.noreply.github.com>
2024-04-25 01:44:23 +0300

    Update sudoers.py

--- AarohiX/plugins/sudo/sudoers.py
+++ AarohiX/plugins/sudo/sudoers.py
@@ -57,7 +57,7 @@ async def sudoers_list(client, message: Message):
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("~ انجب .", show_alert=True)
+        return await callback_query.answer("~ ماكو احد .", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
