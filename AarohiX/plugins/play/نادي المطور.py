5135d612713d8f447406d946057d0f146528211c
zoze <167098056+ayhamvdr@users.noreply.github.com>
2024-04-26 00:55:55 +0300

    Update نادي المطور.py

--- AarohiX/plugins/play/نادي المطور.py
+++ AarohiX/plugins/play/نادي المطور.py
@@ -38,8 +38,8 @@ async def call_dev(client: Client, message: Message):
                                      reply_markup=reply_markup)
 
     # إنشاء زر "اونلاين"
-    online_button = InlineKeyboardButton("< mixthon >", url=f"https://t.me/mixthon")
+    online_button = InlineKeyboardButton("< mixthon >", url=f"https://t.me/muscthon")
     
-    await message.reply_text(f"~ **تم إرسال النداء إلى مطور البوت\n\n-› Master -› @AA37A .",
+    await message.reply_text(f"~ **تم إرسال النداء إلى مطور البوت\n\n-› المطور -› @xx7fxx .",
                              disable_web_page_preview=True,
                              reply_markup=InlineKeyboardMarkup([[online_button]]))
