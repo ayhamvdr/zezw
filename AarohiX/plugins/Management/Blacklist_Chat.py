082e3f903972ff022d4c65a4f59d35ed5575866d
zoze <167098056+ayhamvdr@users.noreply.github.com>
2024-04-25 03:12:59 +0300

    Update Blacklist_Chat.py

--- AarohiX/plugins/Management/Blacklist_Chat.py
+++ AarohiX/plugins/Management/Blacklist_Chat.py
@@ -112,6 +112,6 @@ async def blacklist_filters_re(client, message):  # Added 'client' argument
 
 
 """
-Copyright (c) 2024 @dil_sagar_121
-Copyright (c) 2024 @Alone_Dil_bot
+Copyright (c) 2024 @AA37A
+Copyright (c) 2024 @AA37A
 """
